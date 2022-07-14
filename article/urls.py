9948648a1b867d906c40a368d1e8de8f3e561efc
@islomabdullaev <islom.abdullaev.02@mail.ru>
2022-07-14 15:54:16 +0500

    current affairs page add

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageTemplateView
+from .views import CurrentAffairsTemplateView, HomePageTemplateView
 
 urlpatterns = [
     path("", HomePageTemplateView.as_view(), name="home"),
+    path("current-affairs/", CurrentAffairsTemplateView.as_view(), name="current-affairs"),
 ]
--- article/views.py
+++ article/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomePageTemplateView(TemplateView):
     template_name = "index.html"
+
+
+class CurrentAffairsTemplateView(TemplateView):
+    template_name = "current.html"
